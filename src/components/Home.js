ab5e96fafd741aa988c5fe911f59f02317e581a9
Jeffery <jeffery.ge1234@gmail.com>
2021-06-06 13:52:10 +1000

    changed to blue

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import image from '../background.jpg';
 
 function Home() {
-    return (
-        <h1>Home</h1>
-    )
+  return (
+    <main>
+      <img
+        src={image}
+        alt='mountains'
+        className='absolute object-cover w-full h-full'
+      />
+      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+        <h1 className='text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name'>
+          Hello. I'm Jeffery
+        </h1>
+      </section>
+    </main>
+  );
 }
 
-export default Home
+export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,64 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 function NavBar() {
   return (
-    <header className='bg-red-600'>
+    <header className='bg-blue-600'>
       <div className='container mx-auto flex justify-between'>
         <nav className='flex'>
           <NavLink
             to='/'
             exact
-            activeClassName='text-white'
-            className='inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest'
+            className='inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-blue-800 text-4xl font-bold cursive tracking-widest'
           >
             Jeffery
           </NavLink>
           <NavLink
             to='/post'
-            className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
+            activeClassName='text-blue-100 bg-blue-700'
+            className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-blue-800'
           >
             Blog Posts
           </NavLink>
           <NavLink
             to='/project'
-            className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
+            activeClassName='text-blue-100 bg-blue-700'
+            className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-blue-800'
           >
             Projects
           </NavLink>
           <NavLink
             to='/about'
-            className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
+            activeClassName='text-blue-100 bg-blue-700'
+            className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-blue-800'
           >
             About Me!
           </NavLink>
         </nav>
+        <div className='inline-flex py-3 px-3 my-6'>
+          <SocialIcon
+            url='https://open.spotify.com/user/22aesxj5i6n5osxpg3j7f4kty?si=f32cfa4f6d344455'
+            className='mr-4'
+            target='_blank'
+            fgColor='#fff'
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url='https://www.instagram.com/gege_1175/'
+            className='mr-4'
+            target='_blank'
+            fgColor='#fff'
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url='https://github.com/GeGe1175'
+            className='mr-4'
+            target='_blank'
+            fgColor='#fff'
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
       </div>
     </header>
   );
