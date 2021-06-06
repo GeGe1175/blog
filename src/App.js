b324d1baa1152d72c6b177034d8d21491f33753e
Jeffery <jeffery.ge1234@gmail.com>
2021-06-06 12:22:44 +1000

    redirects

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from 'components/Home';
-import About from 'components/About';
-import SinglePost from 'components/SinglePost';
-import Post from 'components/Post';
-import Project from 'components/Project';
+import Home from './components/Home';
+import About from './components/About';
+import SinglePost from './components/SinglePost';
+import Post from './components/Post';
+import Project from './components/Project';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route component={Home} path='/' exact />
-        <Route component={About} path='/about' />
-        <Route component={SinglePost} path='/post/:slug' />
-        <Route component={Post} path='/post' />
-        <Route component={Project} path='/project' />
+        <Route component={Home} path='/portfolio/' exact />
+        <Route component={About} path='/portfolio/about' />
+        <Route component={SinglePost} path='/portfolio/post/:slug' />
+        <Route component={Post} path='/portfolio/post' />
+        <Route component={Project} path='/portfolio/project' />
       </Switch>
     </BrowserRouter>
   );
